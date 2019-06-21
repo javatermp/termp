4b5ffe04cda0d40726af931295dc712a68f6f7c1
javatermp <51565716+javatermp@users.noreply.github.com>
2019-06-22 00:48:07 +0900

    Update game.java

--- game.java
+++ game.java
@@ -11,30 +11,32 @@ public class game {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PictureChange ch = new PictureChange();
-
+		
 		ch.setTitle("Game");
 		ch.panel1 = new Body1(ch);
 		ch.panel2 = new Body2(ch);
-
+		
 		ch.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ch.getContentPane().add(ch.panel1);
 		ch.pack();
+		ch.setResizable(false);
 		ch.setVisible(true);
 	}
 
 }
 
 class PictureChange extends JFrame {
-
+	
 	Body1 panel1 = null;
 	Body2 panel2 = null;
-
+	
 	public void change(String name) {
-
-		if (name.equals("picture1")) {
+		
+		if(name.equals("picture1")) {
 			getContentPane().removeAll();
 			getContentPane().add(panel1);
 			pack();
+			setResizable(false);
 			setVisible(true);
 			revalidate();
 			repaint();
@@ -42,6 +44,7 @@ class PictureChange extends JFrame {
 			getContentPane().removeAll();
 			getContentPane().add(panel2);
 			pack();
+			setResizable(false);
 			setVisible(true);
 			revalidate();
 			repaint();
