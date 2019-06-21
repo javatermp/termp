51eadb962522b27ed3b4ff76db05f0c6e7ce6322
javatermp <51565716+javatermp@users.noreply.github.com>
2019-06-21 23:00:59 +0900

    Update Body2.java

--- Body2.java
+++ Body2.java
@@ -12,57 +12,87 @@ public class Body2 extends JPanel {
 	private JButton map1, map2, map3, map4;
 	private static JTextArea log;
 	private JScrollPane scroll;
-	public static JLabel gold;
+	public static JLabel gold, exp, item1, item2, item3, item4;
+	private JLabel getitem;
 	private PictureChange ch;
 	static int num;
 	static String text;
-	static int total_gold = 0, total_exp = 0;
+	static boolean judge1 = true;
+	static boolean judge2 = true;
+	static boolean judge3 = true;
+	static boolean judge4 = true;
+	static int current_gold = 0, current_exp = 0;
+	static int current_item1 = 0, current_item2 = 0, current_item3 = 0, current_item4 = 0;
 
 	public Body2(PictureChange temp) {
 		setLayout(null);
 		ch = temp;
 
-		setPreferredSize(new Dimension(600, 600));
-
+		setPreferredSize(new Dimension(800, 700));
+		setBackground(Color.lightGray);
+		
 		log = new JTextArea();
 		log.disable();
 		scroll = new JScrollPane(log);
-		scroll.setLocation(50, 100);
-		scroll.setSize(300, 400);
+		scroll.setLocation(50, 50);
+		scroll.setSize(400, 450);
 		add(scroll);
 		
-		gold = new JLabel("Gold : ");
-		gold.setSize(100, 30);
-		gold.setLocation(100, 30);
+		gold = new JLabel("Gold : 0");
+		gold.setBounds(650,50,130,30);
 		add(gold);
+		
+		exp = new JLabel("Exp : 0");
+		exp.setBounds(500,50,130,30);
+		add(exp);
+		
+		getitem = new JLabel("획득 아이템 : ");
+	    getitem.setBounds(500,100,100,30);
+	    add(getitem);
+		
+		item1 = new JLabel("Item1 : 0");
+		item1.setBounds(500,150,100,30);
+		add(item1);
+		
+		item2 = new JLabel("Item2 : 0");
+		item2.setBounds(500,200,100,30);
+		add(item2);
+		
+		item3 = new JLabel("Item3 : 0");
+		item3.setBounds(500,250,100,30);
+		add(item3);
+		
+		item4 = new JLabel("Item4 : 0");
+		item4.setBounds(500,300,100,30);
+		add(item4);
 
-		button = new JButton("전환2");
-		button.setSize(100, 50);
-		button.setLocation(250, 500);
+		button = new JButton("게임종료");
+		button.setSize(150, 75);
+		button.setLocation(300, 600);
 		button.addActionListener(new ChangeListener());
 		add(button);
 
-		map1 = new JButton("Map1");
-		map1.setSize(80, 50);
-		map1.setLocation(400, 100);
+		map1 = new JButton("맵1");
+		map1.setSize(70, 50);
+		map1.setLocation(500, 350);
 		map1.addActionListener(new Map1Listener());
 		add(map1);
 
-		map2 = new JButton("Map2");
-		map2.setSize(80, 50);
-		map2.setLocation(400, 200);
+		map2 = new JButton("맵2");
+		map2.setSize(70, 50);
+		map2.setLocation(600, 350);
 		map2.addActionListener(new Map2Listener());
 		add(map2);
 
-		map3 = new JButton("Map3");
-		map3.setSize(80, 50);
-		map3.setLocation(400, 300);
+		map3 = new JButton("맵3");
+		map3.setSize(70, 50);
+		map3.setLocation(500, 450);
 		map3.addActionListener(new Map3Listener());
 		add(map3);
 
-		map4 = new JButton("Map4");
-		map4.setSize(80, 50);
-		map4.setLocation(400, 400);
+		map4 = new JButton("맵4");
+		map4.setSize(70, 50);
+		map4.setLocation(600, 450);
 		map4.addActionListener(new Map4Listener());
 		add(map4);
 	}
@@ -75,6 +105,27 @@ public class Body2 extends JPanel {
 	private class ChangeListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
 			ch.change("picture1");
+			map1.setEnabled(true);
+			map2.setEnabled(true);
+			map3.setEnabled(true);
+			map4.setEnabled(true);
+			log.setText(null);
+			judge1 = false;
+			judge2 = false;
+			judge3 = false;
+			judge4 = false;
+			gold.setText("Gold : 0");
+			exp.setText("Exp : 0");
+			item1.setText("Item1 : 0");
+			item2.setText("Item2 : 0");
+			item3.setText("Item3 : 0");
+			item4.setText("Item4 : 0");
+			current_gold = 0;
+			current_exp = 0;
+			current_item1 = 0;
+			current_item2 = 0;
+			current_item3 = 0;
+			current_item4 = 0;
 		}
 	}
 
@@ -130,6 +181,7 @@ public class Body2 extends JPanel {
 class Map1 extends Thread {
 	Random r = new Random();
 	private int map1_gold, map1_exp;
+	static int item1 = 0;
 
 	public Map1() {
 
@@ -138,10 +190,27 @@ class Map1 extends Thread {
 	public synchronized void run() {
 		Body2.addLog("Map1에 입장하였습니다");
 		while (Body2.num == 1) {
+			if(!Body2.judge1) {
+				Body2.judge1 = true;
+				Body2.judge2 = true;
+				Body2.judge3 = true;
+				Body2.judge4 = true;
+				break;
+			}
 			map1_gold = r.nextInt(10) + 1;
-			Body2.total_gold += map1_gold;
-			Body2.addLog(map1_gold);
-			Body2.gold.setText("Gold : " + Body2.total_gold);
+			map1_exp = r.nextInt(10) + 1;
+			Body2.current_gold += map1_gold;
+			Body2.current_exp += map1_exp;
+			if(r.nextInt(10) >= 2) {
+				Body2.addLog(map1_gold + "gold, " + map1_exp + "exp를 획득하였습니다.");
+			} else {
+				item1++;
+				Body2.current_item1 = item1;
+				Body2.addLog(map1_gold + "gold, " + map1_exp + "exp, item1을 1개 획득하였습니다.");
+			}
+			Body2.gold.setText("Gold : " + Body2.current_gold);
+			Body2.exp.setText("Exp : " + Body2.current_exp);
+			Body2.item1.setText("Item1 : " + Body2.current_item1);
 			try {
 				sleep(1000);
 			} catch (Exception e) {
@@ -154,6 +223,7 @@ class Map1 extends Thread {
 class Map2 extends Thread {
 	Random r = new Random();
 	private int map2_gold, map2_exp;
+	static int item2 = 0;
 
 	public Map2() {
 
@@ -162,10 +232,27 @@ class Map2 extends Thread {
 	public synchronized void run() {
 		Body2.addLog("Map2에 입장하였습니다");
 		while (Body2.num == 2) {
+			if(!Body2.judge2) {
+				Body2.judge1 = true;
+				Body2.judge2 = true;
+				Body2.judge3 = true;
+				Body2.judge4 = true;
+				break;
+			}
 			map2_gold = r.nextInt(90) + 11;
-			Body2.total_gold += map2_gold;
-			Body2.addLog(map2_gold);
-			Body2.gold.setText("Gold : " + Body2.total_gold);
+			map2_exp = r.nextInt(90) + 11;
+			Body2.current_gold += map2_gold;
+			Body2.current_exp += map2_exp;
+			if(r.nextInt(10) >= 2) {
+				Body2.addLog(map2_gold + "gold, " + map2_exp + "exp를 획득하였습니다.");
+			} else {
+				item2++;
+				Body2.current_item2 = item2;
+				Body2.addLog(map2_gold + "gold, " + map2_exp + "exp, item2을 1개 획득하였습니다.");
+			}
+			Body2.gold.setText("Gold : " + Body2.current_gold);
+			Body2.exp.setText("Exp : " + Body2.current_exp);
+			Body2.item2.setText("Item2 : " + Body2.current_item2);
 			try {
 				sleep(1000);
 			} catch (Exception e) {
@@ -178,6 +265,7 @@ class Map2 extends Thread {
 class Map3 extends Thread {
 	Random r = new Random();
 	int map3_gold, map3_exp;
+	static int item3 = 0;
 
 	public Map3() {
 
@@ -186,10 +274,27 @@ class Map3 extends Thread {
 	public synchronized void run() {
 		Body2.addLog("Map3에 입장하였습니다");
 		while (Body2.num == 3) {
+			if(!Body2.judge3) {
+				Body2.judge1 = true;
+				Body2.judge2 = true;
+				Body2.judge3 = true;
+				Body2.judge4 = true;
+				break;
+			}
 			map3_gold = r.nextInt(900) + 101;
-			Body2.total_gold += map3_gold;
-			Body2.addLog(map3_gold);
-			Body2.gold.setText("Gold : " + Body2.total_gold);
+			map3_exp = r.nextInt(900) + 101;
+			Body2.current_gold += map3_gold;
+			Body2.current_exp += map3_exp;
+			if(r.nextInt(10) >= 2) {
+				Body2.addLog(map3_gold + "gold, " + map3_exp + "exp를 획득하였습니다.");
+			} else {
+				item3++;
+				Body2.current_item3 = item3;
+				Body2.addLog(map3_gold + "gold, " + map3_exp + "exp, item3을 1개 획득하였습니다.");
+			}
+			Body2.gold.setText("Gold : " + Body2.current_gold);
+			Body2.exp.setText("Exp : " + Body2.current_exp);
+			Body2.item3.setText("Item3 : " + Body2.current_item3);
 			try {
 				sleep(1000);
 			} catch (Exception e) {
@@ -202,6 +307,7 @@ class Map3 extends Thread {
 class Map4 extends Thread {
 	Random r = new Random();
 	private int map4_gold, map4_exp;
+	static int item4 = 0;
 
 	public Map4() {
 
@@ -210,10 +316,27 @@ class Map4 extends Thread {
 	public synchronized void run() {
 		Body2.addLog("Map4에 입장하였습니다");
 		while (Body2.num == 4) {
+			if(!Body2.judge4) {
+				Body2.judge1 = true;
+				Body2.judge2 = true;
+				Body2.judge3 = true;
+				Body2.judge4 = true;
+				break;
+			}
 			map4_gold = r.nextInt(9000) + 1001;
-			Body2.total_gold += map4_gold;
-			Body2.addLog(map4_gold);
-			Body2.gold.setText("Gold : " + Body2.total_gold);
+			map4_exp = r.nextInt(9000) + 1001;
+			Body2.current_gold += map4_gold;
+			Body2.current_exp += map4_exp;
+			if(r.nextInt(10) >= 2) {
+				Body2.addLog(map4_gold + "gold, " + map4_exp + "exp를 획득하였습니다.");
+			} else {
+				item4++;
+				Body2.current_item4 = item4;
+				Body2.addLog(map4_gold + "gold, " + map4_exp + "exp, item4을 1개 획득하였습니다.");
+			}
+			Body2.gold.setText("Gold : " + Body2.current_gold);
+			Body2.exp.setText("Exp : " + Body2.current_exp);
+			Body2.item4.setText("Item4 : " + Body2.current_item4);
 			try {
 				sleep(1000);
 			} catch (Exception e) {
